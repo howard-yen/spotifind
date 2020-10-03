1ab6f12968062a63464ea9de63d6a5c2af895373
Andrew Chen <archen2019@gmail.com>
2020-10-03 01:06:54 -0700

    json to feature vec

--- pyscripts/parse.py
+++ pyscripts/parse.py
@@ -13,4 +13,16 @@ def read_genres():
 
     return genres_dict
 
-
+def parse_track_features(track):
+    audio_features = track['audio_features'][0]
+    feature_vec = np.zeros((9))
+    feature_vec[0] = audio_features['danceability']
+    feature_vec[1] = audio_features['energy']
+    feature_vec[2] = audio_features['loudness']
+    feature_vec[3] = audio_features['speechiness']
+    feature_vec[4] = audio_features['acousticness']
+    feature_vec[5] = audio_features['instrumentalness']
+    feature_vec[6] = audio_features['liveness']
+    feature_vec[7] = audio_features['valence']
+    feature_vec[8] = audio_features['tempo']
+    return feature_vec
---
+++ pyscripts/test.py
@@ -0,0 +1,6 @@
+from parse import *
+from getinfo import *
+
+auth = 'BQBmJX3eD1f_2hLKwoaLJ7uk3GBiea2JuHq8QV5MxFQwzv8oLLlpkMnGVayUbvTNSFG3pWCkXK66VgrmvQVk7ARrrqVBEhq-mn3ehD3m7FCRM3SlRV7CAbHkBCIOFkHFxP2CDSh3wqjZEPZrzB_2yNqzvEUjPKsD23AlaZs7QFU'
+t = get_top_tracks_features(auth)
+print(parse_track_features(t))
