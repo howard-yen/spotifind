c33a77225a40e238516e9d5804d28de7d895319d
howard-yen <howardy2000@gmail.com>
2020-10-03 17:31:24 -0500

    get_neighbors_tracks added to getinfo

--- pyscripts/getinfo.py
+++ pyscripts/getinfo.py
@@ -1,13 +1,13 @@
 import requests
 
-def get_top(auth, tracks=True):
-    url = 'https://api.spotify.com/v1/me/top/{}'
+def get_top(auth, tracks=True, limit=10):
+    url = 'https://api.spotify.com/v1/me/top/{}?limit={limit}'
     headers = {'Accept': 'application/json', 'Content-Type': 'application/json', 'Authorization': 'Bearer {}'.format(auth)}
     if tracks:
         param = 'tracks'
     else:
         param = 'artists'
-    response = requests.get(url.format(param), headers=headers)
+    response = requests.get(url.format(param, limit=limit), headers=headers)
     if response.status_code != 200:
         print("get top didn't work")
         exit()
@@ -58,6 +58,22 @@ def get_top_artists_genres(auth):
     for row in top['items']:
         processed.append(row['genres'])
     return processed
+
+# get the images of the top track for each user
+# return a list of userids, images, and track name
+def get_neighbors_tracks(usertokens, userids):
+    result = {'items': []}
+    for auth, user in zip(usertokens, userids):
+        top = get_top{auth, True, 1)
+        track = top['items'][0]['album']
+        temp = {}
+        temp['userid'] = user
+        temp['image'] = track['images'][0]
+        temp['albumname'] = track['name']
+        temp['url'] = track['external_url']
+        result['items'].append(temp)
+    return result
+
 
 if __name__ == '__main__':
     auth = 'BQChi-zWJcd4DBwhS8yUhi2DYf-TkTqnuj52dAaLaEX9Kk5a0XifTU1t_zo9Ynh2TeUURRCv8d2GN1D0gf1tlodWAgfzHlrM1zddbM_9sUbe97o9kIMc9C65sg25vBN92lSDa5ej05GhFrU-BbkJCiskon0YRG5OQApfx_VizOd5EEX56zEQITfesy0T2QOUGvLTJBfesTCo0ErjDpjCmV7gqAtgkaMjz910VA5A6xT7lCN0PeXTAJRZ80MFLVzj_VbuPBi3lcRc'
