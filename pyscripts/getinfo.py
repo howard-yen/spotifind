f13de03c9b17b05695cf532a71335752c7dab26f
howard-yen <howardy2000@gmail.com>
2020-10-04 00:26:57 -0500

    update getinfo.py

--- pyscripts/getinfo.py
+++ pyscripts/getinfo.py
@@ -39,12 +39,27 @@ def get_top_tracks_features(auth):
     headers = {'Accept': 'aplication/json', 'Content-Type': 'application/json', 'Authorization': 'Bearer {}'.format(auth)}
 
     top = get_top(auth, True)
-    processed = []
     trackids = []
     for row in top['items']:
         trackids.append(row['id'])
 
     response = requests.get(url.format('%2C'.join(trackids)), headers = headers)
+    if response.status_code != 200:
+        print('get top tracks features didnt work')
+        exit()
+
+    return response.json()
+
+def get_features_from_tracks(auth, tracks):
+    url = 'https://api.spotify.com/v1/audio-features?ids={}'
+    headers = {'Accept': 'aplication/json', 'Content-Type': 'application/json', 'Authorization': 'Bearer {}'.format(auth)}
+
+    trackids = []
+    for row in top['itmes']:
+        trackids.append(row['id'])
+
+    response = requests.get(url.format('%2C'.join(trackids)), headers = headers)
+
     if response.status_code != 200:
         print('get top tracks features didnt work')
         exit()
@@ -74,9 +89,20 @@ def get_neighbors_tracks(usertokens, userids):
         result['items'].append(temp)
     return result
 
+# input a list of userids and a list of usertracks json
+def get_neighbors_from_tracks(userids, usertracks):
+    result = {'items': []}
+    for user, tracks in zip(userids, usertracks):
+        track = tracks['items'][0]['album']
+        temp = {}
+        temp['userid'] = user
+        temp['image'] = track['images'][0]
+        temp['albumname'] = track['name']
+        temp['url'] = track['external_url']
+        result['items'].append(temp)
+    return result
 
 if __name__ == '__main__':
     auth = 'BQChi-zWJcd4DBwhS8yUhi2DYf-TkTqnuj52dAaLaEX9Kk5a0XifTU1t_zo9Ynh2TeUURRCv8d2GN1D0gf1tlodWAgfzHlrM1zddbM_9sUbe97o9kIMc9C65sg25vBN92lSDa5ej05GhFrU-BbkJCiskon0YRG5OQApfx_VizOd5EEX56zEQITfesy0T2QOUGvLTJBfesTCo0ErjDpjCmV7gqAtgkaMjz910VA5A6xT7lCN0PeXTAJRZ80MFLVzj_VbuPBi3lcRc'
     t = get_top_artists_genres(auth)
     print(t)
-
