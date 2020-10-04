deaa75a2a7f66c42c7071104589010a7e8d4f2ae
Mathew Han <mathewjhan@gmail.com>
2020-10-04 09:29:14 -0700

    update assets

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Login from "./Login";
+import logo from "../Assets/icon_color.svg";
 import catJAM from "../Assets/catJAM.gif";
 
 const LoginPage = props => {
@@ -9,13 +10,15 @@ const LoginPage = props => {
         <div className="col" />
         <div className="col d-flex align-items-center">
           <div className="card login-container">
+            <div logo="logo">
+            </div>
             <div id="welcome">
               Welcome to Spotifind!
             </div>
             <div className="description">
               Need some new tracks for your mixtape?
             </div>
-            <img id="catjam" src={catJAM} alt="loading..." />
+              <img src={logo} alt="" />
             <div className="description">
               Discover music based on what people are listening to nearby!
             </div>
