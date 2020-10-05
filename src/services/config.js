ebd41df0641f33a8497abcedbf6e5674e6cd0208
Mathew Han <mathewjhan@gmail.com>
2020-10-05 14:33:55 -0700

    remove key

--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,7 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
-export const clientId = "c50e48404d734ba194754be54fb063db";
+export const clientId = process.env.SPOTIFY_CLIENTID;
 export const redirectUri = "https://ivyhacks-spotifind.herokuapp.com";
 export const scopes = [
     "user-top-read",
